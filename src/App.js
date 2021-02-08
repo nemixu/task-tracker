6a1dc8331ef4a3ab459f42f9d90a8f815b43d9ce
nemixu <s.r.seagrave@gmail.com>
2021-02-08 22:01:08 +0000

    added button to components, reusable with props added dynamic styling and text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header'
 function App() {
   return (
     <div className="container">
-        <Header title='ginger bastid' />
+        <Header title='Task Tracker' />
     </div>
   )
 };

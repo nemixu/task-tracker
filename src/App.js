89cd0ae4d5e85eafd865a4e2443d6a2b45eb53e7
nemixu <s.r.seagrave@gmail.com>
2021-02-09 14:17:03 +0000

    added an toggle on double click to show if reminder is necessary

--- src/App.js
+++ src/App.js
@@ -30,10 +30,23 @@ const App = () => {
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
+
+// Toggle reminder
+
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => 
+      task.id === id ? { ...task, reminder:
+      !task.reminder } : task 
+      )
+    )
+}
   return (
     <div className="container">
-        <Header title='Task Tracker' />
-        {tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask} /> : 'No tasks to show'}
+        <Header />
+        {tasks.length > 0 ? (
+          <Tasks tasks={tasks} 
+          onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No tasks to show')}
     </div>
   )
 };

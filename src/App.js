d9c80ef86ee6164149453f3c5ae92a3a2c4f70bc
nemixu <s.r.seagrave@gmail.com>
2021-02-09 12:32:06 +0000

    added tasks to app to handle state at top level

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id:1,
+        text: 'Do some more reacjs',
+        day: 'Feb 16th at 2:30pm',
+        reminder: true, 
+    },
+    {
+        id:2,
+        text: 'Learn sass',
+        day: 'Feb 19th at 2:30pm',
+        reminder: false, 
+    },
+    {
+        id:3,
+        text: 'Go on a walk',
+        day: 'Feb 20th at 3pm',
+        reminder: true, 
+    },
+])
+
+//delete a task
+
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
   return (
     <div className="container">
         <Header title='Task Tracker' />
+        {tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask} /> : 'No tasks to show'}
     </div>
   )
 };
